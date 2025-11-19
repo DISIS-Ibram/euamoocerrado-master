2d8f42f12c62c6ff16de925c6207fed17faffb71
Daniel Figueiredo <docarmorama@gmail.com>
2025-11-19 17:29:06 -0300

    Alterado os arquivos docker-compose, Dockerfile de frontend e vue.config.js de frontend para rodar com hotreload

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,26 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin;
-module.exports = {
-    lintOnSave: false,
-    configureWebpack: {
-        // plugins: [new BundleAnalyzerPlugin()]
-    }
-};
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+//     .BundleAnalyzerPlugin;
+// module.exports = {
+//     lintOnSave: false,
+//     configureWebpack: {
+//         // plugins: [new BundleAnalyzerPlugin()]
+//     }
+// };
 
+
+// hotreload
+module.exports = {
+  lintOnSave: false,
+
+  devServer: {
+    host: "0.0.0.0",
+    port: 8080,
+    hot: true,
+    liveReload: true,
+
+    watchOptions: {
+      poll: 1000,
+      ignored: /node_modules/
+    }
+  }
+};
