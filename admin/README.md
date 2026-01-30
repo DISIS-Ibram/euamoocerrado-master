0b855fc95d511417ac33093ac6a06b961252472c
Daniel Figueiredo <docarmorama@gmail.com>
2026-01-30 14:51:46 -0300

    Inicio da atualizaçao do módulo Admin

--- admin/README.md
+++ admin/README.md
@@ -6,5 +6,14 @@ Para gerar icone svg automaticamente coloque em src/componentes/elements/icon/sv
 mixen box (all t l r b w h)
 
 
+# DANIEL
+Rodar local com a versão 10 - OK
+Rodar no Docker com a versão 10
+
+
+
+Atualizar a versão do Node de 10 para a que der
+
+
 
 
--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -1,118 +1,124 @@
 // http://webpack.github.io/docs/configuration.html
-// http://webpack.github.io/docs/webpack-dev-server.html
-var app_root = 'src'; // the app root folder: src, src_users, etc
+var app_root = 'src';
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 var path = require('path');
-var publicPath = __dirname + '/public/';
 var webpack = require("webpack");
-
 const nib = require('nib');
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     cache: true,
-    devtool:"source-map", //inline-source-map",
+    devtool: "source-map",
+
     entry: [
-        // http://gaearon.github.io/react-hot-loader/getstarted/
-        // 'webpack-dev-server/client?http://localhost:8080',
-        // 'webpack/hot/only-dev-server',
-        __dirname + '/' + app_root + '/index.js',
+        path.join(__dirname, app_root, 'index.js'),
     ],
-    
+
     output: {
-        path: __dirname + '/public/js',
+        path: path.join(__dirname, 'public/js'),
         publicPath: 'js/',
         filename: 'bundle.js',
-        chunkFilename: "bundle.js",
-        // pathinfo:true,
-        library: "[name]"
+        chunkFilename: 'bundle.js',
+        library: '[name]'
     },
-    externals : {
-        lodash : {
+
+    externals: {
+        lodash: {
             commonjs: "lodash",
             amd: "lodash",
-            root: "_" // indicates global variable
+            root: "_"
         }
     },
+
     resolve: {
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
-      alias:{'moment':'moment'},
+        modules: [path.resolve(__dirname, "src"), "node_modules"],
+        alias: { moment: 'moment' },
     },
+
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
-                include: [
-                    path.join(__dirname, "src") //important for performance!
-                ],
+                include: [path.join(__dirname, "src")],
                 query: {
-                    cacheDirectory: true, //important for performance
+                    cacheDirectory: true,
                     presets: ['es2015'],
-                    plugins: ["transform-object-rest-spread",
-                                "transform-class-properties",
-                                "transform-decorators-legacy",
-                                'lodash']
+                    plugins: [
+                        "transform-object-rest-spread",
+                        "transform-class-properties",
+                        "transform-decorators-legacy",
+                        "lodash"
+                    ]
                 }
             },
             {
-              test: /\.svg$/,
-              loader: 'svg-inline-loader'
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
             },
             {
-                // https://github.com/jtangelder/sass-loader
                 test: /\.scss$/,
                 loaders: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
-                // https://github.com/jtangelder/sass-loader
                 test: /\.css$/,
                 loaders: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.styl$/,
                 use: [
-                  'style-loader',
-                  'css-loader',
-                  {
-                    loader: 'stylus-loader',
-                    options: {
-                      use: [nib()],
-                      import: ['~nib/index.styl'] // imports into styl files
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'stylus-loader',
+                        options: {
+                            use: [nib()],
+                            import: ['~nib/index.styl']
+                        }
                     }
-                  }
                 ],
-               
-              },
+            }
         ],
     },
-    devServer: {
-        contentBase: __dirname + '/public',
-        host:'0.0.0.0',
-        port:8080,
-        clientLogLevel: 'info',
-        hot: false,
-        inline: false,
-    },
+
     plugins: [
-        new webpack.ProvidePlugin({
-                     React: "react",
-                     JSONTree:'react-json-tree'
+        // 🔑 Define ambiente de produção (sem -p)
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        new LodashModuleReplacementPlugin,
-          //Typically you'd have plenty of other plugins here as well
+
+        // 🔑 UGLIFY CONTROLADO (ECMA 5)
+        new UglifyJSPlugin({
+            sourceMap: true,
+            uglifyOptions: {
+                ecma: 5,
+                compress: {
+                    warnings: false
+                },
+                output: {
+                    comments: false
+                }
+            }
+        }),
+
+        new webpack.ProvidePlugin({
+            React: "react",
+            JSONTree: 'react-json-tree'
+        }),
+
+        new LodashModuleReplacementPlugin(),
+
         new webpack.DllReferencePlugin({
             context: path.join(__dirname, "src"),
             manifest: require("./dll/vendor-manifest.json")
         }),
 
-        //   new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks 
-        
         new CopyWebpackPlugin([
             { from: 'src/images', to: 'public/images' },
             { from: 'src/themes/dist', to: 'public/themes' }
         ])
-    ],
-
+    ]
 };
